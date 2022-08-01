ed7d85ba6a74381bc264108bcb51e3377294f75d
giovanafurlan <giovananelofurlan@gmail.com>
2022-08-01 14:40:15 -0300

    Ajustando permissões

--- setCookie.php
+++ setCookie.php
@@ -1,7 +1,7 @@
 <?
 setcookie("MyCookie", "subinsb.com", time()+3600);
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: https://127.0.0.1:5000");
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Methods: GET, POST");
-header("Access-Control-Allow-Headers: Content-Type, *");
+header("Access-Control-Allow-Headers: Content-Type");
 ?>
